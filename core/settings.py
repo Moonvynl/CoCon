262b9b617a6749cb6abc4b208798c6b819eb7a11
David <david2009mn@gmail.com>
2024-08-23 20:19:56 +0300

    fix

--- core/settings.py
+++ core/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',
                         'allauth.account.auth_backends.AuthenticationBackend', )
 
-SITE_ID = 2 
+SITE_ID = 3
 
 SOCIALACCOUNT_PROVIDERS = { 'google': { 'SCOPE': [ 'profile', 'email', ], 'AUTH_PARAMS': { 'access_type': 'online', } } }
 
