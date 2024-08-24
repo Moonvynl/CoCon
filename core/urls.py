a69c59d346c48cab3663b85ac52a994a0ceb3296
David <david2009mn@gmail.com>
2024-08-24 12:43:37 +0300

    fix

--- core/urls.py
+++ core/urls.py
@@ -30,3 +30,6 @@ urlpatterns = [
     path('', include('notification_system.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
