3538b0c0232882ef1c92493898da8dbae7bf3487
David <david2009mn@gmail.com>
2024-08-24 12:51:20 +0300

    fix

--- core/settings.py
+++ core/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-91-ejse89x0iv))40z6*a47*-vkyz@@o)#ul6%#e0!u33($$5a'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -108,7 +108,7 @@ DATABASES = {
 }
 database_url = os.environ.get('DATABASE_URL')
 DATABASES = {
-    "default": dj_database_url.parse('postgresql://cogito_connect_db_user:aDXB4asKmf8MImJnERaXEXdWeXrtRMGu@dpg-cr492vt6l47c73bd3ndg-a.oregon-postgres.render.com/cogito_connect_db')
+    "default": dj_database_url.parse(database_url)
 }
 
 
